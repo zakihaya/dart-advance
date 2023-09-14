77752c07763c4e92eb6609d14a38a0b8670437cb
hayazaki <hayazaki_toru@saleshub.jp>
2023-09-14 14:46:48 +0900

    コンストラクタとsuper

---
+++ constructor/01-default_constructor.dart
@@ -0,0 +1,10 @@
+// 「引数無し」「デフォルトコンストラクタ定義あり」
+class Person {
+  Person() {
+    print('Personクラスのインスタンスが生成されました');
+  }
+}
+
+void main() {
+  Person person = Person();
+}
---
+++ constructor/README.md
@@ -0,0 +1,4 @@
+# コンストラクタとsuperについて
+
+参考
+https://zenn.dev/tsukatsuka1783/articles/flutter_constract_super
